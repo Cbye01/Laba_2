ffe3a7b7cfddc20a215678efa4f5db8524739fdb
Cbye01 <146924389+Cbye01@users.noreply.github.com>
2023-12-17 21:02:55 +0200

    Point.cs

--- Task_2/Point.cs
+++ Task_2/Point.cs
@@ -1,6 +1,27 @@
-﻿namespace Lab2;
+namespace Lab2;
 
 public class Point
 {
-    
+    public double point1
+    {
+        get;
+    }
+
+    public double point2
+    {
+        get;
+    }
+
+    public string point3
+    {
+        get;
+    }
+        
+
+    public Point(double point1_, double point2_ , string point3_)
+    {
+        point1 = point1_;
+        point2 = point2_;
+        point3 = point3_;
+    }
 }
